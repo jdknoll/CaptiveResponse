2802358ed74350127b552b7775461eba0503a76a
lclbemosior <lclbemosior@sandbox.ship.edu>
2013-11-19 15:39:21 -0500

    Adding in CAS (it's broken, but I'm branching it off)

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,6 +33,17 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
   
-  public $components = array('DebugKit.Toolbar');
+    public $components = array(
+        'DebugKit.Toolbar',
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
+
+    public function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
 
 }
